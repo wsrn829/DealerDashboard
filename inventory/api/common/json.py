d5cc2dceb77b63d3104a287d8fedb9afbf5c7be7
wlongmire <warrenlongmire@gmail.com>
2023-04-28 11:52:51 -0400

    adds date to common/json.py encoder.

--- inventory/api/common/json.py
+++ inventory/api/common/json.py
@@ -1,16 +1,15 @@
 from json import JSONEncoder
 from django.urls import NoReverseMatch
 from django.db.models import QuerySet
-from datetime import datetime
+from datetime import datetime, date
 
 
 class DateEncoder(JSONEncoder):
     def default(self, o):
-        if isinstance(o, datetime):
+        if isinstance(o, (datetime, date)):
             return o.isoformat()
         else:
             return super().default(o)
-
 
 class QuerySetEncoder(JSONEncoder):
     def default(self, o):
--- service/api/common/json.py
+++ service/api/common/json.py
@@ -1,12 +1,12 @@
 from json import JSONEncoder
 from django.urls import NoReverseMatch
 from django.db.models import QuerySet
-from datetime import datetime
+from datetime import datetime, date
 
 
 class DateEncoder(JSONEncoder):
     def default(self, o):
-        if isinstance(o, datetime):
+        if isinstance(o, (datetime, date)):
             return o.isoformat()
         else:
             return super().default(o)
